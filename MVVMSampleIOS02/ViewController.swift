438a33faa2a739fcf0f97ec0b7e2cc51a15ad8d9
velo.yamigiku@gmail.com <velo.yamigiku@gmail.com>
2020-10-21 16:34:01 +0900

    ・新規作成。

---
+++ MVVMSampleIOS02/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVMSampleIOS02
+//
+//  Created by xda-yoshioka on 2020/10/21.
+//  Copyright © 2020 velo.yamigiku. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
