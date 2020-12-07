b37b3fbde42670243e5d08ac09234481a70fe5e5
velo.yamigiku@gmail.com <velo.yamigiku@gmail.com>
2020-12-07 16:24:52 +0900

    ・コレクションビューのセルのUI作成・配置をコード実装にする。

--- MVVMSampleIOS02/CollectionViewCell.swift
+++ MVVMSampleIOS02/CollectionViewCell.swift
@@ -10,8 +10,8 @@ import UIKit
 
 class CollectionViewCell: UICollectionViewCell {
     
-    @IBOutlet weak var label: UILabel!
-    @IBOutlet weak var name: UILabel!
+    var label: UILabel!
+    var name: UILabel!
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
@@ -20,5 +20,28 @@ class CollectionViewCell: UICollectionViewCell {
         self.layer.borderColor = UIColor.black.cgColor
         self.layer.cornerRadius = 8
         
+        label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(label)
+        name = UILabel()
+        name.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(name)
+        
     }
+    
+    override func layoutSubviews() {
+        
+        label.topAnchor.constraint(equalTo: self.topAnchor, constant: 5).isActive = true
+        label.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 5).isActive = true
+        label.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -5).isActive = true
+        label.heightAnchor.constraint(equalToConstant: 60).isActive = true
+        
+        name.topAnchor.constraint(equalTo: label.bottomAnchor, constant: 5).isActive = true
+        name.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 5).isActive = true
+        name.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -5).isActive = true
+        name.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -5).isActive = true
+        
+    }
+    
+    
 }
